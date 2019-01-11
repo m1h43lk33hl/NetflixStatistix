e20b85c7d9dadfb6ff41e47b03ddf5dc3562f2cc
Jordy van Ekelen <jordyvanekelen1@gmail.com>
2019-01-11 23:56:45 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class NetflixStatistixUI implements Runnable
+{
+    @Override
+    public void run() {
+
+    }
+}
